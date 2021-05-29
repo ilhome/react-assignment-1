be116cdd062cc2d3de14e7ed86b3d2ea25e64672
ilhome <ilkhom9799@gmail.com>
2021-05-30 07:07:28 +0900

    bar is created

---
+++ src/components/Chart/Chart.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ChartBar from "./ChartBar";
+import "./ChartBar.css";
+
+const Chart = (props) => {
+  return <div className="chart"></div>;
+};
+
+export default Chart;
